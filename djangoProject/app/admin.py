13a0bf7a990e8be31264dac31f3ec5b8db6782d8
Luann Rodrigo de Souza Santos <94115637+luannsantos321@users.noreply.github.com>
2023-01-31 07:16:19 -0300

    Delete djangoProject directory

--- djangoProject/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Produto
-# Register your models here.
-
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('produto')
-admin.site.register(Produto)
