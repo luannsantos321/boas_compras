680836e4bad0ba665ed3a6ac85b8032a23e59401
luannsantos321 <luannsantostec@gmail.com>
2023-02-01 20:40:58 -0300

    adicionado valor na lista

--- boas_compras/app/urls.py
+++ boas_compras/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import inicio, lista, cadastro, \
-    update_produto, delete_produto, delete_produto_all, entrada_valores, valor,delete_valor
+    update_produto, delete_produto, delete_produto_all, cadastro_valor,delete_valor, soma
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('lista/', lista,  name='lista'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('update_produto/<int:id>/', update_produto, name='update_produto'),
     path('delete_produto/<int:id>/', delete_produto, name='delete_produto'),
     path('delete_produto_all/', delete_produto_all, name='delete_produto_all'),
-    path('entrada_valores/', entrada_valores, name='entrada_valores'),
-    path('valor/', valor, name='valor'),
+    path('cadastro_valor/', cadastro_valor, name='cadastro_valor'),
     path('delete_valor/<int:id>/', delete_valor, name='delete_valor'),
+    path('soma/', soma,  name='soma'),
 ]
--- boas_compras/app/views.py
+++ boas_compras/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from .models import Produto,Valor
 from .forms import ProdutoForm, ValorForm
 # Create your views here.
@@ -7,7 +8,8 @@ def inicio(request):
 
 def lista(request):
     lista = Produto.objects.all()
-    return render(request, 'lista.html', {'lista': lista})
+    valor = Valor.objects.all()
+    return render(request, 'lista.html', {'lista': lista, 'valor': valor})
 
 
 def cadastro(request):
@@ -16,7 +18,9 @@ def cadastro(request):
     if form.is_valid():
         form.save()
         return redirect('lista')
-    return render(request, 'cadastro.html', {'form': form})
+
+    return render(request, 'cadastro.html', {'form': form, })
+
 
 def update_produto(request,id):
     lista = Produto.objects.get(id=id)
@@ -26,6 +30,7 @@ def update_produto(request,id):
         form.save()
         return redirect('lista')
     return render(request, 'cadastro.html', {'form': form, 'lista': lista})
+
 
 def delete_produto(request, id):
     lista = Produto.objects.get(id=id)
@@ -42,8 +47,26 @@ def delete_produto_all(request):
         lista.delete()
         return redirect('lista')
     return render(request, 'cadastro.html')
+'''---------------------------------------------'''
 
+def cadastro_valor(request):
+    form2 = ValorForm(request.POST or None)
+    if form2.is_valid():
+        form2.save()
+        return redirect('lista')
+    return  render(request, 'cadastro_valor.html',{'form2': form2})
 
+def delete_valor(request, id):
+    valor = Valor.objects.get(id=id)
 
+    if request.method == 'POST':
+        valor.delete()
+        return redirect('lista')
+    return render(request, 'cadastro_valor.html')
 
-
+def soma(request):
+    valor = Valor.objects.all()
+    total = 0.00
+    for valor in valor:
+        total += valor
+    return HttpResponse(request, 'lista.html', {'total': total})
